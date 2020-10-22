56b48e2f95cc44c8d934e07d285f9e21103b445b
crispaulcastaneda <cristianpaul_c@yahoo.com>
2020-10-22 21:48:19 +0800

    Updates

---
+++ precache-manifest.2e3d43f0865811755f99ac47d94270d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc37934cb250310328f76068d341c371",
+    "url": "/react-currency-converter/index.html"
+  },
+  {
+    "revision": "239d72f938078a85ee7b",
+    "url": "/react-currency-converter/static/css/main.6e9431e6.chunk.css"
+  },
+  {
+    "revision": "b17d0b2d5837470f73d5",
+    "url": "/react-currency-converter/static/js/2.e5c5968a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-currency-converter/static/js/2.e5c5968a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "239d72f938078a85ee7b",
+    "url": "/react-currency-converter/static/js/main.0f99d7ff.chunk.js"
+  },
+  {
+    "revision": "0e9b31f3e8aeaacb1a34",
+    "url": "/react-currency-converter/static/js/runtime-main.3a71fbf3.js"
+  }
+]);
